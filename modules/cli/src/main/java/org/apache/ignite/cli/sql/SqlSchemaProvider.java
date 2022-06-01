a4d4be3c8699a62d535a69c787945c59d2723bc9
Mikhail <Pochatkin@users.noreply.github.com>
2022-06-01 22:13:56 +0300

    Update sql schema by timer (#37)
    * Update sql schema by timer

--- modules/cli/src/main/java/org/apache/ignite/cli/sql/SqlSchemaProvider.java
+++ modules/cli/src/main/java/org/apache/ignite/cli/sql/SqlSchemaProvider.java
@@ -1,95 +1,35 @@
 package org.apache.ignite.cli.sql;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * SQL schema provider.
  */
 public class SqlSchemaProvider implements SchemaProvider {
-    private final MetadataSupplier metadataSupplier;
-    private Map<String, Map<String, Set<String>>> schema;
+    private static final int SCHEMA_UPDATE_TIMEOUT = 10;
+
+    private final SqlSchemaLoader sqlSchemaLoader;
+    private final int schemaUpdateTimeout;
+    private SqlSchema schema;
+    private Instant lastUpdate;
 
     public SqlSchemaProvider(MetadataSupplier metadataSupplier) {
-        this.metadataSupplier = metadataSupplier;
+        this(metadataSupplier, SCHEMA_UPDATE_TIMEOUT);
+    }
+
+    SqlSchemaProvider(MetadataSupplier metadataSupplier, int schemaUpdateTimeout) {
+        sqlSchemaLoader = new SqlSchemaLoader(metadataSupplier);
+        this.schemaUpdateTimeout = schemaUpdateTimeout;
     }
 
     @Override
-    public Map<String, Map<String, Set<String>>> getSchema() {
-        if (schema == null) {
-            schema = new HashMap<>();
-            try (ResultSet rs = metadataSupplier.getMetaData().getTables(null, null, null, null)) {
-                while (rs.next()) {
-                    String tableSchema = rs.getString("TABLE_SCHEM");
-                    Map<String, Set<String>> tables = schema.computeIfAbsent(tableSchema, schemaName -> new HashMap<>());
-                    tables.put(rs.getString("TABLE_NAME"), null);
-                }
-            } catch (SQLException e) {
-                //todo report error
-            }
+    public SqlSchema getSchema() {
+        if (schema == null || Duration.between(lastUpdate, Instant.now()).toSeconds() >= schemaUpdateTimeout) {
+            schema = sqlSchemaLoader.loadSchema();
+            lastUpdate = Instant.now();
         }
         return schema;
     }
 
-    @Override
-    public Set<String> getColumnNames(String tableName) {
-        Entry<String, Map<String, Set<String>>> schema = findSchema(tableName);
-        if (schema != null) {
-            Map<String, Set<String>> tables = schema.getValue();
-            return tables.computeIfAbsent(tableName, key -> getColumns(schema.getKey(), key));
-        }
-        return Collections.emptySet();
-    }
-
-    /**
-     * Retrieves column names from the metadata.
-     *
-     * @param schemaName name of the schema.
-     * @param tableName name of the table.
-     * @return set of column names.
-     */
-    private Set<String> getColumns(String schemaName, String tableName) {
-        Map<String, Set<String>> tables = schema.get(schemaName);
-        if (tables != null) {
-            if (!tables.containsKey(tableName)) {
-                try (ResultSet rs = metadataSupplier.getMetaData().getColumns(null, schemaName, tableName, null)) {
-                    while (rs.next()) {
-                        Set<String> columns = tables.computeIfAbsent(tableName, key -> new HashSet<>());
-                        columns.add(rs.getString("COLUMN_NAME"));
-                    }
-                } catch (SQLException e) {
-                    //todo report error
-                }
-            }
-            return tables.getOrDefault(tableName, Collections.emptySet());
-        }
-        return Collections.emptySet();
-    }
-
-    /**
-     * Finds table schema which contains a table with the given name.
-     *
-     * @param tableName name of the table to find.
-     * @return map entry for the schema or null if table is not found.
-     */
-    private Entry<String, Map<String, Set<String>>> findSchema(String tableName) {
-        Map<String, Map<String, Set<String>>> schema = getSchema();
-        for (Entry<String, Map<String, Set<String>>> entry : schema.entrySet()) {
-            if (entry.getValue().containsKey(tableName)) {
-                return entry;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public void invalidateSchema() {
-        schema = null;
-    }
 }
